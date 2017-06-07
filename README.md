73e718cffc143b089cb48b85a0fafe7127fb7a39
Francois Ferrari <f.ferrari@wanadoo.fr>
2017-06-07 19:18:41 +0200

    Refactoring of FixedPriceAuctionActorsSpec into one file

--- README.md
+++ README.md
@@ -6,20 +6,20 @@
     
 | ReservePrice? | Renewal? | Bidders | Test file            | Should be                                                                      |
 |---------------|----------|---------|----------------------|--------------------------------------------------------------------------------|
-| Yes           | No       | No      | AuctionActorSpec1    | Closed when end-time is reached, without winner                                |
-| Yes           | Yes      | No      | AuctionActorSpec2    | Restarted when end-time is reached, without winner                             |
-| Yes           | -        | A       | AuctionActorSpec3    | Closed when end-time is reached, winner A for his single bid                   |
-| Yes           | -        | A,B     | AuctionActorSpec4    | Closed when end-time is reached, winner B                                      |
-| No            | No       | A       | AuctionActorSpec5    | Closed when end-time is reached, without winner (highest bid < reserve price   |
-| No            | No       | A       | AuctionActorSpec6    | Closed when end-time is reached, winner A (bid = reserve price)                |
-| No            | No       | A       | AuctionActorSpec7    | Closed when end-time is reached, winner A (bid > reserve price)                |
+| Yes           | No       | No      | AuctionActorSpec     | Closed when end-time is reached, without winner                                |
+| Yes           | Yes      | No      | AuctionActorSpec     | Restarted when end-time is reached, without winner                             |
+| Yes           | -        | A       | AuctionActorSpec     | Closed when end-time is reached, winner A for his single bid                   |
+| Yes           | -        | A,B     | AuctionActorSpec     | Closed when end-time is reached, winner B                                      |
+| No            | No       | A       | AuctionActorSpec     | Closed when end-time is reached, without winner (highest bid < reserve price   |
+| No            | No       | A       | AuctionActorSpec     | Closed when end-time is reached, winner A (bid = reserve price)                |
+| No            | No       | A       | AuctionActorSpec     | Closed when end-time is reached, winner A (bid > reserve price)                |
 |               |          |         |                      |                                                                                |
 
 #### Fixed price
 
-| Renewal? | Bidders | Test file                      | Should be                                                                      |
-|----------|---------|--------------------------------|--------------------------------------------------------------------------------|
-| No       | No      | FixedPriceAuctionActorSpec1    | Closed and not sold                                                            |
-| Yes      | No      | FixedPriceAuctionActorSpec2    | Restarted and not sold                                                         |
-| -        | A       | FixedPriceAuctionActorSpec3    | Closed and sold for the total stock available                                  |
-| -        | A       | FixedPriceAuctionActorSpec4    | Closed and sold for the requested qty, cloned for the remaining stock          |
+| Renewal? | Bidders | Test file                  | Should be                                                                      |
+|----------|---------|----------------------------|--------------------------------------------------------------------------------|
+| No       | No      | FixedPriceAuctionActorSpec | Closed and not sold                                                            |
+| Yes      | No      | FixedPriceAuctionActorSpec | Restarted and not sold                                                         |
+| -        | A       | FixedPriceAuctionActorSpec | Closed and sold for the total stock available                                  |
+| -        | A       | FixedPriceAuctionActorSpec | Closed and sold for the requested qty, CLONED for the remaining stock          |
--- test/actors/auction/FixedPriceAuctionActorSpec.scala
+++ test/actors/auction/FixedPriceAuctionActorSpec.scala
@@ -4,26 +4,29 @@ import java.time.Instant
 
 import actors.ActorCommonsSpec
 import actors.auction.AuctionActor._
-import actors.auction.fsm.{ClosedState, FinishedAuction}
+import actors.auction.fsm.{ActiveAuction, ClosedState, FinishedAuction, StartedState}
 import actors.auctionSupervisor.AuctionSupervisor
 import actors.user.UserActor.UserRegisteredReply
 import actors.userSupervisor.UserSupervisor
 import actors.userUnicity.UserUnicityActor
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, PoisonPill, Props}
+import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestKit}
+import akka.util.Timeout
 import cqrs.UsersBid
-import cqrs.commands._
+import cqrs.commands.{CreateAuction, CreateUser, GetCurrentState, PlaceBid}
 import models.{AuctionReason, BidRejectionReason}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import play.api.inject.BindingKey
 import play.api.inject.guice.GuiceApplicationBuilder
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
 /**
   * Created by Francois FERRARI on 17/05/2017
   */
-class FixedPriceAuctionActorSpec3() extends TestKit(ActorSystem("AuctionSystem"))
+class FixedPriceAuctionActorSpec() extends TestKit(ActorSystem("AuctionSystem"))
   with ActorCommonsSpec
   with ImplicitSender
   with WordSpecLike
@@ -44,17 +47,7 @@ class FixedPriceAuctionActorSpec3() extends TestKit(ActorSystem("AuctionSystem")
   val seller = makeUser("contact@pluto.space", "hhgg", "Robert", "John")
   val bidderA = makeUser("bidderA@pluto.space", "bidderA", "BidderA", "John")
 
-  val auction = makeFixedPriceAuction(
-    startPrice = 0.10,
-    startsAt = Instant.now().plusSeconds(2),
-    lastsSeconds = 20,
-    stock = 10,
-    hasAutomaticRenewal = false,
-    sellerId = seller.userId
-  )
-
-  "A scheduled FIXED PRICE auction" should {
-
+  "A FIXE PRICE auction" should {
     "successfully create a User (Seller)" in {
       userSupervisorActorRef ! CreateUser(seller)
       expectMsg(UserRegisteredReply)
@@ -64,6 +57,78 @@ class FixedPriceAuctionActorSpec3() extends TestKit(ActorSystem("AuctionSystem")
       userSupervisorActorRef ! CreateUser(bidderA)
       expectMsg(UserRegisteredReply)
     }
+  }
+
+  "A scheduled FIXED PRICE auction W/O bids W/O automatic renewal" should {
+
+    lazy val auction = makeFixedPriceAuction(
+      startPrice = 0.10,
+      startsAt = Instant.now().plusSeconds(2),
+      lastsSeconds = 5,
+      stock = 10,
+      hasAutomaticRenewal = false,
+      sellerId = seller.userId
+    )
+
+    "successfully create an auction in StartedState" in {
+      auctionSupervisorActorRef ! CreateAuction(auction)
+      expectMsg(AuctionScheduledReply)
+    }
+
+    "be in CLOSED state and NOT SOLD when endsAt is reached" in {
+      expectNoMsg(secondsToWaitForAuctionEnd(auction).seconds)
+
+      getAuctionActorSelection(auction.auctionId) ! GetCurrentState
+      expectMsgPF() {
+        case CurrentStateReply(ClosedState, FinishedAuction(finishedAuction))
+          if finishedAuction.bids.isEmpty &&
+            !finishedAuction.isSold
+        => ()
+      }
+    }
+  }
+
+  "A scheduled FIXED PRICE auction W/O bids W/automatic renewal" should {
+
+    lazy val auction = makeFixedPriceAuction(
+      startPrice = 0.10,
+      startsAt = Instant.now().plusSeconds(2),
+      lastsSeconds = 5,
+      stock= 10,
+      hasAutomaticRenewal = true,
+      sellerId = seller.userId
+    )
+
+    "successfully create an auction in StartedState" in {
+      auctionSupervisorActorRef ! CreateAuction(auction)
+      expectMsg(AuctionScheduledReply)
+    }
+
+    "be RESTARTED and NOT SOLD when endsAt is reached" in {
+      expectNoMsg(secondsToWaitForAuctionEnd(auction).seconds)
+
+      getAuctionActorSelection(auction.auctionId) ! GetCurrentState
+      expectMsgPF() {
+        case CurrentStateReply(StartedState, ActiveAuction(activeAuction))
+          if activeAuction.bids.isEmpty &&
+            activeAuction.startsAt == auction.endsAt &&
+            activeAuction.endsAt.isAfter(auction.endsAt) &&
+            !activeAuction.isSold
+        => ()
+      }
+    }
+  }
+
+  "A scheduled FIXED PRICE auction W/bids" should {
+
+    lazy val auction = makeFixedPriceAuction(
+      startPrice = 0.10,
+      startsAt = Instant.now().plusSeconds(2),
+      lastsSeconds = 20,
+      stock = 10,
+      hasAutomaticRenewal = false,
+      sellerId = seller.userId
+    )
 
     "successfully create an auction in ScheduledState" in {
       auctionSupervisorActorRef ! CreateAuction(auction)
@@ -152,4 +217,77 @@ class FixedPriceAuctionActorSpec3() extends TestKit(ActorSystem("AuctionSystem")
     //      expectMsg(10.seconds, BidPlacedReply)
     //    }
   }
+
+  "A FIXED PRICE auction W/bids" should {
+
+    val originalStock = 10
+    val requestedStock = 8
+
+    lazy val auction = makeFixedPriceAuction(
+      startPrice = 0.10,
+      startsAt = Instant.now(),
+      lastsSeconds = 30,
+      stock = originalStock,
+      hasAutomaticRenewal = false,
+      sellerId = seller.userId
+    )
+
+    implicit val askTimeout = Timeout(2.seconds)
+
+    "successfully create an auction in StartedState" in {
+      auctionSupervisorActorRef ! CreateAuction(auction)
+      expectMsg(AuctionStartedReply)
+    }
+
+    "accept a bid on an auction for a part of the stock" in {
+      getAuctionActorSelection(auction.auctionId) ! PlaceBid(UsersBid(auction.auctionId, bidderA.nickName, bidderA.userId, requestedStock, auction.currentPrice, Instant.now()))
+      expectMsg(BidPlacedReply)
+    }
+
+    "successfully check that the auction is closed and that it contains a reference to the cloned auction" in {
+      expectNoMsg(2.seconds)
+
+      getAuctionActorSelection(auction.auctionId) ! GetCurrentState
+      expectMsgPF() {
+        case CurrentStateReply(ClosedState, FinishedAuction(finishedAuction))
+          if finishedAuction.bids.length == 1 &&
+            finishedAuction.stock == 0 &&
+            finishedAuction.originalStock == requestedStock &&
+            finishedAuction.isSold &&
+            finishedAuction.clonedToAuctionId.isDefined => ()
+      }
+    }
+
+    "successfully check that the cloned auction is in StartedState and that it contains a reference to its parent auction with the remaining stock" in {
+      expectNoMsg(2.seconds)
+
+      Await.result((getAuctionActorSelection(auction.auctionId) ? GetCurrentState).mapTo[CurrentStateReply], 2.seconds) match {
+        case CurrentStateReply(ClosedState, FinishedAuction(finishedAuction)) =>
+          // Get the CurrentState of the cloned auction
+          getAuctionActorSelection(finishedAuction.clonedToAuctionId.get) ! GetCurrentState
+
+          expectMsgPF() {
+            case CurrentStateReply(StartedState, ActiveAuction(clonedAuction))
+              if clonedAuction.clonedFromAuctionId.contains(finishedAuction.auctionId) &&
+                clonedAuction.clonedToAuctionId.isEmpty &&
+                clonedAuction.endsAt == finishedAuction.endsAt &&
+                clonedAuction.stock == (originalStock-requestedStock) &&
+                clonedAuction.originalStock == (originalStock-requestedStock) => ()
+          }
+
+          // To make the cloned auction unsubscribe from the seller actor's events
+          getAuctionActorSelection(finishedAuction.clonedToAuctionId.get) ! PoisonPill
+
+        case _ =>
+          fail
+      }
+    }
+
+    "wait for auction actor end" in {
+      // To make the original auction unsubscribe from the seller actor's events
+      getAuctionActorSelection(auction.auctionId) ! PoisonPill
+      expectNoMsg(2.seconds)
+    }
+  }
+
 }
